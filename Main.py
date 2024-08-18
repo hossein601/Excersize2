4ca50b13663c8cc27b4a527c22d9c948485e875e
Hossein <hkargar17@gmail.com>
2024-08-18 06:08:28 -0700

    this is new

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
 from restaurant import Restaurant
-
 restaurant = Restaurant(input('Enter your resturan name'), input('Enter your restaurant address'))
 restaurant.menu.add_food('kebab', 200)
 restaurant.menu.add_food('gheime', 150)
@@ -7,4 +6,4 @@ restaurant.menu.add_food('ghorme', 250)
 restaurant.add_staff("sara", "chef")
 restaurant.add_staff("ali", "assistance chef")
 restaurant.show_oder()
-restaurant.get_order(input('please enter your name'), ['gheime', 'ghorme'])
+restaurant.get_order(input('please enter your name'),['gheime','ghorme'])
